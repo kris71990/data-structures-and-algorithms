014fc2c556ba5100c34f8b38888783edea5720ee
Kris Sakarias <ksakarias@gmail.com>
2018-04-17 15:59:28 -0700

    finished tests

--- shift-array/__test__/shift-array.test.js
+++ shift-array/__test__/shift-array.test.js
@@ -3,8 +3,13 @@
 const shift = require('../shift-array');
 
 describe('shift-array.js', () => {
-  test('Should return array with value in middle', () => {
+  test('Odd length array should return value in middle', () => {
     expect(shift([1, 2, 3, 4], 5)).toEqual([1, 2, 5, 3, 4]);
+  });
+  test('Even length array should return value in middle in middle - 1', () => {
     expect(shift([1, 2, 3, 4, 5], 8)).toEqual([1, 2, 8, 3, 4, 5]);
+  });
+  test('Should return array with value in middle', () => {
+    expect(shift([], 5)).toEqual([5]);
   });
 });
