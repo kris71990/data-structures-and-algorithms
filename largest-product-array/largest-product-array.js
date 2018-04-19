1f8e35692b7e19cfc331061361b4516e51c59a03
Kris Sakarias <ksakarias@gmail.com>
2018-04-19 15:36:45 -0700

    largest product problem complete

---
+++ largest-product-array/largest-product-array.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      let down;
+      let right;
+      const current = arr[i][j];
+      if (arr[i + 1]) right = arr[i + 1][j];
+      if (arr[j + 1]) down = arr[i][j + 1];
+      if (right && right * current > sum) sum = right * current;
+      if (down && down * current > sum) sum = down * current;
+    }
+  }
+  return sum;
+};
