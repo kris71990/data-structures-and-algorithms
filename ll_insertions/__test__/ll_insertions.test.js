755bdd48855ec50adbf907d8ea915bdeffc3b6fc
Kris Sakarias <ksakarias@gmail.com>
2018-04-24 00:00:35 -0700

    readme completed

---
+++ ll_insertions/__test__/ll_insertions.test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const LinkedList = require('../ll_insertions');
+
+describe('linked-list insertions', () => {
+  test('Should add the given value at end of list', () => {
+    const test = new LinkedList();
+    test.append(4);
+    expect(test.head.value).toEqual(4);
+    test.append(5);
+    expect(test.head.next.value).toEqual(5);
+    test.append(6);
+    expect(test.head.next.next.value).toEqual(6);
+  });
+
+  test('Should insert second argument value node before first argument value node', () => {
+    const test = new LinkedList();
+    test.append(4);
+    test.append(5);
+    test.insertBefore(5, 3);
+    expect(test.head.next.value).toEqual(3);
+    test.insertBefore(5, 2);
+    expect(test.head.next.next.value).toEqual(2);
+    test.insertBefore(2, 8);
+    expect(test.head.next.next.value).toEqual(8);
+  });
+
+  test('Should insert second argument value node after first argument value node', () => {
+    const test = new LinkedList();
+    test.append(4);
+    test.insertAfter(4, 3);
+    expect(test.head.value).toEqual(4);
+    test.insertAfter(3, 6);
+    expect(test.head.next.next.value).toEqual(6);
+    test.insertAfter(6, 8);
+    expect(test.head.next.next.next.value).toEqual(8);
+  });
+});
---
+++ ll_insertions/ll_insertions.js
@@ -0,0 +1,75 @@
+'use strict';
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+module.exports = class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+  }
+
+  insertBefore(value, newValue) {
+    const node = new Node(newValue);
+
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+    let currentNode = this.head;
+    let curr = currentNode.next;
+
+    while (curr) {
+      if (curr.value === value) {
+        currentNode.next = node;
+        node.next = curr;
+        return this;
+      }
+    
+      currentNode = curr;
+      curr = curr.next;
+    }
+    return null;
+  }
+
+  insertAfter(value, newValue) {
+    const node = new Node(newValue);
+
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        const temp = currentNode.next;
+        currentNode.next = node;
+        currentNode.next.next = temp;
+        break;
+      }
+      currentNode = currentNode.next;
+    }
+    return this;
+  }
+};
