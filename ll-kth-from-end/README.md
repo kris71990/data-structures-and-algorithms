34e3dc79470434b7970f05f23fdbe0ce4e29308a
Kris Sakarias <ksakarias@gmail.com>
2018-04-24 14:25:44 -0700

    ll-kth from end finished and tests passing

---
+++ ll-kth-from-end/README.md
@@ -0,0 +1,7 @@
+# Linked List - Find kth from end
+
+## Challenge
+Given a Linked List class, return the node that is k nodes from the end of the list.
+
+## Solution
+![](../assets/ll_kth-from-end.jpg)
---
+++ ll-kth-from-end/kth-from-end.js
@@ -0,0 +1,46 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+module.exports = class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+  }
+
+  kthFromEnd(k) {
+    if (!this.head) return null;
+    let currentNode = this.head;
+    let counterNode = this.head;
+    let length = 0;
+    while (currentNode) {
+      currentNode = currentNode.next;
+      length += 1;
+    }
+
+    for (let i = 0; i < length - k - 1; i++) {
+      counterNode = counterNode.next;
+    }
+    return counterNode;
+  }
+};
