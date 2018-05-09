b5ad3309f49246a681fa18980e1d0424a533f6c0
Kris Sakarias <ksakarias@gmail.com>
2018-05-08 20:58:00 -0700

    finished breadth traversal, tests passing

---
+++ breadth-traversal/README.md
@@ -0,0 +1,7 @@
+# Breadth Traversal of Binary Tree
+
+## Challenge
+Traverse a binary tree by level from root to leaf.
+
+## Solution
+![](../assets/breadth-traversal.jpg)
---
+++ breadth-traversal/breadth-traversal.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import Queue from './queue';
+
+function breadthTraversal(rootNode) {
+  const queue = new Queue();
+  const traversedNodes = [];
+  queue.enqueue(rootNode);
+
+  while (!queue.isEmpty()) {
+    const first = queue.storage[queue.storage.length - 1];
+    traversedNodes.push(first.value);
+    queue.dequeue();
+    if (first.left) queue.enqueue(first.left);
+    if (first.right) queue.enqueue(first.right);
+  }
+  console.log(traversedNodes);
+  return traversedNodes;
+}
+
+export default breadthTraversal;
