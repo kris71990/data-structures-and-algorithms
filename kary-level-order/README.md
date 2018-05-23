6b7a79968fdb5127eaa510968cd0f49b6442e7b4
Kris Sakarias <ksakarias@gmail.com>
2018-05-22 18:27:53 -0700

    kary level traversal complete

---
+++ kary-level-order/README.md
@@ -0,0 +1,7 @@
+# K-ary tree level traversal
+
+## Challenge
+This function must traverse a k-ary tree and return a string of each level of the tree separated by a new line.
+
+## Solution
+![](../assets/kary-level-traversal.jpg)
---
+++ kary-level-order/queue.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// This queue works
+const LinkedList = require('./linked-list');
+
+module.exports = class Queue {
+  constructor() {
+    this.storage = new LinkedList();
+  }
+
+  enqueue(value) {
+    return this.storage.insertAtHead(value);
+  }
+
+  dequeue() {
+    return this.storage.pop();
+  }
+
+  isEmpty() {
+    if (!this.storage.head) return true;
+    return false;
+  }
+};
